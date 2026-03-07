62d7cdbaec07639ae55a2aa6de7e38d77ae16c90
tanishq singhal <tanishaksinghal785@gmail.com>
2026-03-07 23:34:03 +0530

    setup the app and implement homepage and new book ui

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import HeroSection from "@/components/HeroSection";
+
+const Page = () => {
+
+
+    return (
+        <main className="wrapper container">
+            <HeroSection />
+        </main>
+    )
+}
+
+export default Page
